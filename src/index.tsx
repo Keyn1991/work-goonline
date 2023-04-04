a59c46f7d6519e1258f106907508e90c309f714e
potap <potapchuk50@gmail.com>
2023-04-04 07:02:55 +0100

    feat

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import {App} from './App';
-import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -13,4 +13,4 @@ root.render(
 );
 
 
-reportWebVitals();
+
